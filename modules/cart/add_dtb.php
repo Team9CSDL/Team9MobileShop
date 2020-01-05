56b0683105d02a4df0e7d5238a778a3639350356
NguyenVanGiang29 <58686853+NguyenVanGiang29@users.noreply.github.com>
2020-01-05 10:41:34 +0700

    Update add_dtb.php

--- modules/cart/add_dtb.php
+++ modules/cart/add_dtb.php
@@ -1,8 +1,10 @@
 <?php
     $date = date('Y-m-d H:i:s');
-    $product=$id['prd_id'];
-	$amount=$quantity['amount'];
-    $sql = "INSERT INTO order_list(ord_name, ord_phone, ord_address,ord_mail, ord_date, prd_id, amount ,ord_status, ord_total)
-    VALUES('$name', '$phone', '$address', '$email', '$date', '$product', '$amount' ,0, $total_cash)";
+    $product='';
+    foreach($_SESSION['cart'] as $id => $quantity){
+        $product .= $id . ':' . $quantity . ';';
+    }
+    $sql = "INSERT INTO order_list(ord_name, ord_phone, ord_address,ord_mail, ord_date, ord_list, ord_status, ord_total)
+    VALUES('$name', '$phone', '$address', '$email', '$date', '$product', 0, $total_cash)";
     mysqli_query($con, $sql);
 ?>
